ba522537c7b4335ec4e568da92f573c184467dc2
Abubakar <sidi364@gmail.com>
2021-12-21 18:00:52 +0500

    Custom Authentication Server

---
+++ Custom Autherization Server/src/main/java/com/pp/customautherizationserver/Configs/AuthServerConfig.java
@@ -0,0 +1,46 @@
+package com.pp.customautherizationserver.Configs;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+
+@Configuration
+@EnableAuthorizationServer
+public class AuthServerConfig
+        extends AuthorizationServerConfigurerAdapter {
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @Override
+    public void configure(AuthorizationServerSecurityConfigurer security) throws Exception {
+        security.passwordEncoder(NoOpPasswordEncoder.getInstance())
+                .checkTokenAccess("isAuthenticated()"); // isAuthenticated()
+
+    }
+
+    @Override
+    public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
+        clients.inMemory()
+                .withClient("client1")
+                .secret("secret1")
+                .scopes("read")
+                .accessTokenValiditySeconds(5000) // seconds
+                .authorizedGrantTypes("authorization_code", "refresh_token")
+                .redirectUris("http://localhost:8080/home");
+
+    }
+
+    @Override
+    public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
+        endpoints.authenticationManager(authenticationManager);
+    }
+}
---
+++ Custom Autherization Server/src/main/java/com/pp/customautherizationserver/CustomAutherizationServerApplication.java
@@ -0,0 +1,19 @@
+package com.pp.customautherizationserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@SpringBootApplication
+//@Configuration
+//@EnableAutoConfiguration
+//@ComponentScan
+public class CustomAutherizationServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CustomAutherizationServerApplication.class, args);
+    }
+
+}
