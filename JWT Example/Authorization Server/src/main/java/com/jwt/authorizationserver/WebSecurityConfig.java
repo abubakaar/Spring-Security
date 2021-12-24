3124994e492b80ba57f0dd5468066994e7db6ef9
Abubakar <sidi364@gmail.com>
2021-12-24 18:07:02 +0500

    Authrozation server Sending JWT token

---
+++ JWT Example/Authorization Server/src/main/java/com/jwt/authorizationserver/WebSecurityConfig.java
@@ -0,0 +1,37 @@
+package com.jwt.authorizationserver;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+@Configuration
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Bean
+    public UserDetailsService uds() {
+        var uds = new InMemoryUserDetailsManager();
+        var u = User.withUsername("johny")
+                .password("12345")
+                .authorities("read")
+                .build();
+        uds.createUser(u);
+        return uds; }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return NoOpPasswordEncoder.getInstance();
+    }
+    @Bean
+    public AuthenticationManager authenticationManagerBean()
+            throws Exception {
+        return super.authenticationManagerBean();
+    }
+}
